490bf80640e10ffbf1b01972e688eeefb9f08182
Saurabh Shiradkar <72598771+saurabhshiradkar@users.noreply.github.com>
2024-03-23 16:13:59 +0530

    changed deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,35 @@
+
+
 #!/bin/bash
 
-# copy to project dir
-cp -r * /var/nodeapp/react-jenkins-demo
+# Destination directory
+DEST_DIR="/var/nodeapp/react-jenkins-demo"
 
-# navigate to dir
-cd /var/nodeapp/react-jenkins-demo
+# Clean the destination directory
+rm -rf $DEST_DIR/*
 
-# find the process pid of instance 300
+# Copy files from Jenkins workspace to destination directory
+cp -r /var/lib/jenkins/workspace/nodeapp/* $DEST_DIR/
+
+# Navigate to the destination directory
+cd $DEST_DIR
+
+# Find the process PID of instance 3000
 PID=$(lsof -t -i :3000)
 
-# if the pid is not empty then kill it
+# If the PID is not empty then kill it
 if [ -n "$PID" ]; then 
     su -c "kill -9 $PID" -s /bin/bash root
 fi
 
-# stop pm2 process
-pm2 stop ecosystem.config.js
+# Stop pm2 process
+sudo pm2 stop ecosystem.config.js
 
-# install nodejs
-npm install
+# Install Node.js dependencies
+sudo npm install
 
-# start app with pm2
-pm2 start ecosystem.config.js
+# Start app with pm2
+sudo pm2 start ecosystem.config.js
 
-# output
-echo "The Deployment is successful, running on port 3000 with new pid of instance & pm2"
+# Output
+echo "The Deployment is successful, running on port 3000 with a new PID of instance & pm2"
