1abb71bc1776de185f392f633599aec45593d06b
Saurabh Shiradkar <72598771+saurabhshiradkar@users.noreply.github.com>
2024-03-23 13:43:20 +0530

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,27 @@
-# !/bin/bash
+#!/bin/bash
 
 # copy to project dir
-
 cp -r * /var/nodeapp/react-jenkins-demo
 
 # navigate to dir
 cd /var/nodeapp/react-jenkins-demo
 
-#  find the process pid of instance 300
+# find the process pid of instance 300
 PID=$(lsof -t -i :3000)
 
-
-# if the pid not empty then kill it
-if [-n "$PID"]; then 
-su -c "kill -9 $PID"  -s /bin/bash root]
+# if the pid is not empty then kill it
+if [ -n "$PID" ]; then 
+    su -c "kill -9 $PID" -s /bin/bash root
 fi
 
 # stop pm2 process
 pm2 stop ecosystem.config.js
 
 # install nodejs
-npm install 
+npm install
 
-#  start app with pm2
+# start app with pm2
 pm2 start ecosystem.config.js
 
 # output
-echo "The Deployment is successfully, running on port 3000 with new pid of instance & pm2"
-
+echo "The Deployment is successful, running on port 3000 with new pid of instance & pm2"
